2210629d04f2446a702beee6275eed09cfe3d44e
Xue <xuemin2505@gmail.com>
2016-12-06 02:12:01 -0500

    updated project

--- java.js
+++ java.js
@@ -2,94 +2,175 @@
  * Created by admin on 11/13/16.
  */
 
+//Global Variable
+var user;
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA5kBeLf09lwZBx_hFa0j73_2BaoKpfJZ8",
+    authDomain: "cp317timetrack.firebaseapp.com",
+    databaseURL: "https://cp317timetrack.firebaseio.com",
+    storageBucket: "cp317timetrack.appspot.com",
+    messagingSenderId: "800102312098"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
+
 $( document ).on( "pageinit", function() {
-
-
-
-
+//LOGIN BUTTON
     $("#login").click(function () {
-            var email = $("#email").val();
+            user = $("#user").val();
             var password = $("#password").val();
-// Checking for blank fields.
-            if (email == '' || password == '') {
+            var database=firebase.database();
+
+
+
+// Checking for blank fields. LOGIN BUTTON
+            if (user == '' || password == '') {
                 $('input[type="text"],input[type="password"]').css("border", "2px solid red");
                 $('input[type="text"],input[type="password"]').css("box-shadow", "0 0 3px red");
-                alert("Please fill all fields...!!!!!!");
-            } else {
-                $.post("login.php", {email1: email, password1: password},
-                    function (data) {
-                        if (data == 'Invalid Email.......') {
-                            $('input[type="text"]').css({"border": "2px solid red", "box-shadow": "0 0 3px red"});
-                            $('input[type="password"]').css({
-                                "border": "2px solid #00F5FF",
-                                "box-shadow": "0 0 5px #00F5FF"
-                            });
-                            alert(data);
-                        } else if (data == 'Email or Password is wrong...!!!!') {
-                            $('input[type="text"],input[type="password"]').css({
-                                "border": "2px solid red",
-                                "box-shadow": "0 0 3px red"
-                            });
-                            alert(data);
-                        } else if (data == 'Successfully Logged in...') {
-                            $("form")[0].reset();
-                            $('input[type="text"],input[type="password"]').css({
-                                "border": "2px solid #00F5FF",
-                                "box-shadow": "0 0 5px #00F5FF"
-                            });
-                            alert(data);
-                        } else {
-                            alert(data);
-                        }
-                    });
+                alert("Please fill all fields");
+            }
+            else {
+
+                firebase.database().ref('users/'+user).on("value",function(snapshot) {
+                    var realPassword = snapshot.val().pass;
+
+                    if (password == realPassword){
+
+                        $.mobile.changePage("modifyHours.html",{
+                            transition: "pop",
+                            reverse: false,
+                            changeHash: true
+                        })
+
+
+
+                    }
+                    else{
+                        alert("Enter the correct password")
+                    }
+
+
+                });
             }
         });
 
-
+//THIS IS THE SIGNUP PAGE, NEW USER INPUTS INFO AND PRESSES BUTTON DONE
     $("#done").click(function () {
         var first = $("#fname").val();
         var last = $("#lname").val();
         var id = $("#id").val();
         var code = $("#code").val();
-        var demail = $("#demail").val();
+        var duser = $("#duser").val();
         var pass = $("#pass").val();
         var cpass = $("#cpassword").val();
-        if (first == '' || last == '' || id == '' || code == '' || demail == '' || pass == '' || cpass == '') {
+
+
+        if (first == '' || last == '' || id == '' || code == '' || duser == '' || pass == '' || cpass == '') {
             alert("Please fill in the form");
         }
-
         else {
-            $.mobile.changePage("test.html", {
-                transition: "pop",
-                reverse: false,
-                changeHash: true
+                var database = firebase.database();
+                firebase.database().ref("users/"+duser).set({
+                    user: duser,
+                    pass:pass,
+                    Amonday:0,
+                    Atuesday:0,
+                    Awednesday:0,
+                    Athursday:0,
+                    Afriday:0,
+                    Asaturday:0,
+                    Asunday:0,
+                    Wmonday:0,
+                    Wtuesday:0,
+                    Wwednesday:0,
+                    Wthursday:0,
+                    Wfriday:0,
+                    Wsaturday:0,
+                    Wsunday:0
+
+                })
+                $.mobile.changePage("index.html", {
+                    transition: "pop",
+                    reverse: false,
+                    changeHash: true
+                });
 
 
-            });
+
+
+
+
         }
     });
 
+//SIGNUP BUTTON IN INDEX PAGE
+    $("#signup").click(function () {
+        $.mobile.changePage("Signup.html", {
+            transition: "pop",
+            reverse: false,
+            changeHash: true
+
+        });
+
+    });
 
 
+//SUBMIT BUTTON IN MODIFYHOURS PAGE
     $("#submit").click(function() {
-        var smon=$("#s1").val();
-        var stue=$("#s2").val();
-        var swed=$("#s3").val();
-        var sth=$("#s4").val();
-        var sfr=$("#s5").val();
-        var sst=$("#s6").val();
-        var ssun=$("#s7").val();
-        var emon=$("#e1").val();
-        var etue=$("#e2").val();
-        var ewed=$("#e3").val();
-        var eth=$("#e4").val();
-        var efr=$("#e5").val();
-        var est=$("#e6").val();
-        var esun=$("#e7").val();
-        alert("Your hours have been submitted!");
+        var mon=$("#s1").val();
+        var tue=$("#s2").val();
+        var wed=$("#s3").val();
+        var thu=$("#s4").val();
+        var fri=$("#s5").val();
+        var sat=$("#s6").val();
+        var sun=$("#s7").val();
+    if (checkHour(mon)&&checkHour(tue)&&checkHour(wed)&&checkHour(thu)&&checkHour(fri)&&checkHour(sat)&&checkHour(sun)){
+        var database = firebase.database();
+        firebase.database().ref("users/"+user).update({
+            Amonday:mon,
+            Atuesday:tue,
+            Awednesday:wed,
+            Athursday:thu,
+            Afriday:fri,
+            Asaturday:sat,
+            Asunday:sun
+        })
+        alert("Availability hours have been updated successfully!");
+    }
+    else{
+
+        alert("There is a problem with the submitted hours :(");
+
+    }
+
     })
 
 
+    //This weeks schedule display update
+    $("#hours").click(function(){
+
+        var database = firebase.database();
+        firebase.database().ref("users/"+user).on("value", function(snapshot){
+            $('#m').html(snapshot.val().Wmonday);
+            $('#t').html(snapshot.val().Wtuesday);
+            $('#w').html(snapshot.val().Wwednesday);
+            $('#th').html(snapshot.val().Wthursday);
+            $('#f').html(snapshot.val().Wfriday);
+            $('#s').html(snapshot.val().Wsaturday);
+            $('#sn').html(snapshot.val().Wsunday);
+        })
+
+
+    })
+
+    //makes sure that a given number is between 0 and 24
+function checkHour(number){
+        return (number>-1 && number<25);
+}
 
 
 
